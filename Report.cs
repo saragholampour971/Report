c6ed38366980641da176769a218d27f8e21b4512
sara <saragholampour971@yahoo.com>
2024-02-07 11:49:24 +0330

    init

---
+++ Report.cs
@@ -0,0 +1,32 @@
+﻿namespace Report;
+
+public interface ISubMenu
+{
+    string Label { get; }
+
+    Func<string> Result { get;  }
+}
+
+public interface IReport<T>
+{
+    public string Label { get;  }
+
+    public List<T> Items { get; }
+
+    public List<ISubMenu> SubMenus { get;  }
+}
+
+public class SubMenu : ISubMenu
+{
+    public string Label { get; }
+
+    public Func<string> Result { get; }
+
+
+    public SubMenu(string label, Func<string> result)
+    {
+        Label = label;
+        Result = result;
+    }
+}
+
